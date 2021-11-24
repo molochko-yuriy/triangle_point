c5d2e506b153ed1e38fef58142a87adca32b2626
Molochko <molochko.uriy@gmail.com>
2021-11-24 22:57:47 +0300

    changing service

--- src/main/java/by/company/molochko/triangle_point/service/TriangleService.java
+++ src/main/java/by/company/molochko/triangle_point/service/TriangleService.java
@@ -1,65 +1,90 @@
 package by.company.molochko.triangle_point.service;
 
-import by.company.molochko.triangle_point.exception.IncorrectAnglesSumException;
+import by.company.molochko.triangle_point.entity.Point;
+import by.company.molochko.triangle_point.entity.Triangle;
+import by.company.molochko.triangle_point.exception.TriangleException;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class TriangleService {
-    private static final double CONSTANT_VALUE = 0.5;
+    private static final String ERROR_MESSAGE = "Error data! Null triangle!";
 
-    public double calculatePerimeter(double a, double b, double c) {
-        return a + b + c;
+    public double findSquare(Triangle triangle) throws TriangleException {
+        if (triangle == null) {
+            throw new TriangleException(ERROR_MESSAGE);
+        }
+        List<Double> sides = findSides(triangle);
+        double halfPerimeter = findPerimeter(triangle) * 0.5;
+        return Math.sqrt(halfPerimeter * (halfPerimeter - sides.get(0))
+                * (halfPerimeter - sides.get(1)) * (halfPerimeter - sides.get(2)));
     }
 
-    public double calculateSquare(double height, double base) {
-        return CONSTANT_VALUE * height * base;
+    private List<Double> findSides(Triangle triangle) {
+        return new ArrayList<Double>() {{
+            add(findSide(triangle.getPointA(), triangle.getPointB()));
+            add(findSide(triangle.getPointB(), triangle.getPointC()));
+            add(findSide(triangle.getPointA(), triangle.getPointC()));
+        }};
     }
 
-    public boolean isRectangular(int firstCorner, int secondCorner, int thirdCorner) throws IncorrectAnglesSumException {
-        if (firstCorner + secondCorner + thirdCorner != 180) {
-            throw new IncorrectAnglesSumException("Sum should be 180");
-        }
-        if (firstCorner == 90 || secondCorner == 90 || thirdCorner == 90) {
-            System.out.println("треугольник прямоугольный");
-            return true;
-        }
-        return false;
+    private double findSide(Point pointA, Point pointB) {
+        return Math.sqrt(Math.pow(pointB.getX() - pointA.getX(), 2) + Math.pow(pointB.getY() - pointA.getY(), 2));
     }
 
-    public boolean isIsosceles(int firsSide, int secondSide, int thirdSide) {
-        if (firsSide == secondSide || secondSide == thirdSide || thirdSide == firsSide) {
-            System.out.println("треугольник равнобедренный");
-            return true;
+    public double findPerimeter(Triangle triangle) throws TriangleException {
+        if (triangle == null) {
+            throw new TriangleException(ERROR_MESSAGE);
         }
-        return false;
+        List<Double> sides = findSides(triangle);
+        return sides.stream().mapToDouble(side -> side).sum();
     }
 
-    public boolean isEquilateral(int firsSide, int secondSide, int thirdSide) {
-        if (firsSide == secondSide && secondSide == thirdSide) {
-            System.out.println("треугольник равносторонний");
-            return true;
+    public boolean isIsosceles(Triangle triangle) throws TriangleException {
+        if (triangle == null) {
+            throw new TriangleException(ERROR_MESSAGE);
         }
-        return false;
+        List<Double> sides = findSides(triangle);
+        return checkEquality(sides.get(0), sides.get(1)) || checkEquality(sides.get(0), sides.get(2))
+                || checkEquality(sides.get(1), sides.get(2));
     }
 
-    public boolean isAcuteAngled(int firstCorner, int secondCorner, int thirdCorner) throws IncorrectAnglesSumException {
-        if (firstCorner + secondCorner + thirdCorner != 180) {
-            throw new IncorrectAnglesSumException("Sum should be 180");
-        }
-        if (firstCorner < 90 && secondCorner < 90 && thirdCorner < 90) {
-            System.out.println("треугольник остроугольный");
-            return true;
-        }
-        return false;
+    private boolean checkEquality(Object a, Object b) {
+        return (a == b) || (a != null && a.equals(b));
     }
 
-    public  boolean  isObtuse(int firstCorner, int secondCorner, int thirdCorner) throws IncorrectAnglesSumException {
-        if (firstCorner + secondCorner + thirdCorner != 180) {
-            throw new IncorrectAnglesSumException("Sum should be 180 ");
+    public boolean isEquilateral(Triangle triangle) throws TriangleException {
+        if (triangle == null) {
+            throw new TriangleException(ERROR_MESSAGE);
         }
-        if(firstCorner > 90 || secondCorner > 90 || thirdCorner > 90) {
-            System.out.println("треугольник тупоугольный");
-            return true;
-        }
-        return false;
+        List<Double> sides = findSides(triangle);
+        return checkEquality(sides.get(0), sides.get(1)) && checkEquality(sides.get(0), sides.get(2));
     }
 
+    public boolean isRectangular(Triangle triangle) throws TriangleException {
+        if (triangle == null) {
+            throw new TriangleException(ERROR_MESSAGE);
+        }
+        List<Double> sides = findSides(triangle);
+        boolean first = sides.get(0) == Math.sqrt(Math.pow(sides.get(1), 2) + Math.pow(sides.get(2), 2));
+        boolean second = sides.get(1) == Math.sqrt(Math.pow(sides.get(0), 2) + Math.pow(sides.get(2), 2));
+        boolean third = sides.get(2) == Math.sqrt(Math.pow(sides.get(1), 2) + Math.pow(sides.get(0), 2));
+        return first || second || third;
+    }
+
+    public boolean isAcute(Triangle triangle) throws TriangleException {
+        if (triangle == null) {
+            throw new TriangleException(ERROR_MESSAGE);
+        }
+        List<Double> sides = findSides(triangle);
+        return Math.pow(sides.get(0), 2) + Math.pow(sides.get(1), 2) > Math.pow(sides.get(2), 2);
+    }
+
+    public boolean isObtuse(Triangle triangle) throws TriangleException {
+        if (triangle == null) {
+            throw new TriangleException(ERROR_MESSAGE);
+        }
+        List<Double> sides = findSides(triangle);
+        return Math.pow(sides.get(0), 2) + Math.pow(sides.get(1), 2) < Math.pow(sides.get(2), 2);
+    }
 }
